e0097950618b23063556d173e17f2d5ed5a9d5db
Mykola Skorenkyi <mskor@softserveinc.com>
2016-01-19 14:13:18 +0200

    finish base install

--- base_install.sh
+++ base_install.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
-DISK="/dev/sda"
-SWAP="16GiB"
-
 echo "Step 1: Prepare the hard drive"
 
 dd if=/dev/zero of=/dev/sda bs=512 count=1
 
-parted ${DISK} mklabel msdos --script
-parted ${DISK} mkpart primary linux-swap 1MiB ${SWAP} --script
-parted ${DISK} mkpart primary ext4 ${SWAP} 100% --script
-parted ${DISK} set 2 boot on --script
+parted /dev/sda mklabel msdos --script
+parted /dev/sda mkpart primary linux-swap 1MiB 16GiB --script
+parted /dev/sda mkpart primary ext4 16GiB 100% --script
+parted /dev/sda set 2 boot on --script
 
-mkswap ${DISK}1
-swapon ${DISK}1
-mkfs.ext4 ${DISK}2 -F
+mkswap /dev/sda1
+swapon /dev/sda1
+mkfs.ext4 /dev/sda2 -F
 
 echo "Step 2: Install system"
 
-mount ${DISK}2 /mnt
+mount /dev/sda2 /mnt
 pacstrap -i /mnt base base-devel --noconfirm
 genfstab -U /mnt /mnt/etc/fstab
 
 echo "Step 3: Configure system"
-sed -i "/#en_US.UTF-8/ s/# *//" /mnt/etc/locale.gen
-echo "LANG=en_US.UTF-8" > /mnt/etc/locale.conf
 
 arch-chroot /mnt /bin/bash -c '
+sed -i "/#en_US.UTF-8/ s/# *//" /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+export LANG=en_US.UTF-8
 locale-gen
+
+ln -s /usr/share/zoneinfo/Europe/Kiev > /etc/localtime
+hwclock --systohc --utc
+
+pacman -S grub os-prober --noconfirm
+grub-install --recheck --target=i386-pc /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+echo cyberpunk > /etc/hostname
+echo "root:password" | chpasswd
 ' # END OF CHROOT
+
+umount -R /mnt
+reboot
 
